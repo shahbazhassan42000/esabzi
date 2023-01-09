69b4c429c9ae842e441b06ccc5d48bfbbea5cc64
shahbazhassan42000 <shahabzhassan42000@gmail.com>
2023-01-09 22:16:57 +0500

    updated project to ASP.net MVC structure

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace esabzi.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public string Index()
+        {
+            return "Auth Controller";
+        }
+
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Signup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using esabzi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace esabzi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
